42e8c78cb365498e143516d604b29685911405b0
elliotfiske <elliotfiske@gmail.com>
2015-02-27 19:05:42 -0800

    init title screen state, added .swp files to gitignore

---
+++ www/src/title-state.js
@@ -0,0 +1,21 @@
+var TitleScreen = function() {
+    var title = {};
+
+    /**
+     * Update the title screen! What do we even DO here???
+     *
+     * title screen animations or something?
+     */
+    title.update = function() {
+        
+    }
+
+    title.init = function() {
+        document.addEventListener("mousemove". function(evt) {
+            console.log(evt);
+        });
+    }
+
+
+
+};
