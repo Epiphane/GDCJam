86cf3bbf0a7f2d138d4f6281f2a71313f6dd9b8d
Epiphane <exyphnos@gmail.com>
2015-02-27 21:07:11 -0800

    Added SAT for collision

--- www/src/in-game.js
+++ www/src/in-game.js
@@ -3,63 +3,84 @@
  *  bounceTime lets us animate bounciness on mouse down.
  */
 function Paddle(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.x2 = function() { return this.x + this.width };
-    this.y2 = function() { return this.y + this.height };
+    this.x2 = function() { return this.shape.pos.x + this.width };
+    this.y2 = function() { return this.shape.pos.y + this.height };
     this.height = height;
     this.width = width;
     this.bounceTime = 0;
+
+    this.shape = new SAT.Box(new SAT.Vector(x, y), width, height).toPolygon();
 }
 
 Paddle.prototype.draw = function(context) {
+    this.shape.pos.x = this.shape.pos.x;
+    this.shape.pos.y = this.shape.pos.y;
+
     context.fillStyle = this.color || "rgb(200, 200, 200)";
-    context.fillRect(this.x, this.y, this.width, this.height);
+    context.fillRect(this.shape.pos.x, this.shape.pos.y, this.width, this.height);
 }
  
 /**
  * Circle class defines those funky circles that emanate out from
  *  where an intersection happens.
  */
-function Ball(x, y, radius, alpha) {
-    this.x = x;
-    this.y = y;
-    this.radius = 40;
-    this.alpha = 0.4;
-}
- 
-/**
- * rectIntersection takes in two rectangle objects and determines whether they
- *  intersect.
- *
- * If they intersect, this function returns the rectangle that represents the area
- *  they intersect. Otherwise, it returns null.
- */
-function rectIntersection(rectA, rectB) {
-    var intersectTop    = Math.max(rectA.y,    rectB.y);
-    var intersectRight  = Math.min(rectA.x2(), rectB.x2());
-    var intersectBottom = Math.min(rectA.y2(), rectB.y2());
-    var intersectLeft   = Math.max(rectA.x,    rectB.x);
- 
-    var intersectWidth  = intersectRight - intersectLeft;
-    var intersectHeight = intersectBottom - intersectTop;
- 
-    if (intersectWidth > 0 && intersectHeight > 0) {
-        return new Paddle(intersectLeft, intersectTop, intersectWidth, intersectHeight);
+function Ball(x, y, radius, speed) {
+    this.radius = radius;
+
+    var direction = Math.random() > 0.5;
+    var widthOfAngle = Math.PI / 2;
+    var angle = Math.random() * widthOfAngle - widthOfAngle / 2;
+
+    this.shape = new SAT.Circle(new SAT.Vector(x, y), radius);
+
+    this.velocity = {
+        x: speed * Math.cos(angle) * (direction ? 1 : -1),
+        y: speed * Math.sin(angle),
     }
-    return null;
 }
- 
-/**
- * rectContainsPoint returns true if the specified rectangle contains the point,
- *  false otherwise.
- */
-function rectContainsPoint(rect, point) {
-    if (point.x > rect.x && point.x < rect.x2() &&
-        point.y > rect.y && point.y < rect.y2() ) {
-        return true;
+
+Ball.prototype.update = function(game) {
+    this.shape.pos.x += this.velocity.x;
+    this.shape.pos.y += this.velocity.y;
+
+    if (this.shape.pos.y - this.radius <= 0) {
+        this.shape.pos.y = 2 * this.radius - this.shape.pos.y;
+        this.velocity.y *= -1;
     }
-    return false;
+    else if (this.shape.pos.y + this.radius >= canvas.height) {
+        var offset = this.shape.pos.y + this.radius - canvas.height;
+        this.shape.pos.y = this.shape.pos.y - 2 * offset;
+        this.velocity.y *= -1;
+    }
+
+    // Check for paddle collisions
+    var collision = new SAT.Response();
+    if (SAT.testPolygonCircle(game.player1.shape, this.shape, collision)) {
+        this.shape.pos.x += collision.overlapV.x * 2;
+        if (collision.overlapV.x)
+            this.velocity.x *= -1;
+
+        this.shape.pos.y += collision.overlapV.y * 2;
+        if (collision.overlapV.y)
+            this.velocity.y *= -1;
+    }
+    if (SAT.testPolygonCircle(game.player2.shape, this.shape, collision)) {
+        this.shape.pos.x += collision.overlapV.x * 2;
+        if (collision.overlapV.x)
+            this.velocity.x *= -1;
+
+        this.shape.pos.y += collision.overlapV.y * 2;
+        if (collision.overlapV.y)
+            this.velocity.y *= -1;
+    }
+};
+
+Ball.prototype.draw = function(context) {
+    context.beginPath();
+    context.arc(this.shape.pos.x, this.shape.pos.y, 
+                this.radius, 0, 2*Math.PI, false);
+    context.fillStyle = "rgba(255, 255, 255, 1)";
+    context.fill();
 }
 
 /** 
@@ -83,7 +104,7 @@ function InGame() {
     // Initial variables
     var distFromEdge = 20;
     var initialSize = { w: 20, h: 140 };
-    var ballSize = 30;
+    var ballSize = 20;
     this.speed = 8;
 
     // "Entities"
@@ -91,10 +112,11 @@ function InGame() {
     this.player2 = new Paddle(canvas.width - distFromEdge - initialSize.w, canvas.height / 2, initialSize.w, initialSize.h);
     this.ball = new Ball(canvas.width / 2 - ballSize / 2,
                          canvas.height / 2 - ballSize / 2,
-                         ballSize, ballSize);
+                         ballSize, 10);
 }
 
 InGame.prototype.init = function() {
+    // this.ball.update(this);
 };
 
 /**
@@ -103,23 +125,21 @@ InGame.prototype.init = function() {
  */
 InGame.prototype.update = function() {
     if (keyDown[KEYS.UP])
-        this.player2.y -= this.speed;
+        this.player2.shape.pos.y -= this.speed;
     if (keyDown[KEYS.DOWN])
-        this.player2.y += this.speed;
+        this.player2.shape.pos.y += this.speed;
 
     if (keyDown[KEYS.W])
-        this.player1.y -= this.speed;
+        this.player1.shape.pos.y -= this.speed;
     if (keyDown[KEYS.S])
-        this.player1.y += this.speed;
+        this.player1.shape.pos.y += this.speed;
+
+    this.ball.update(this);
 };
 
 InGame.prototype.draw = function(context) {
     this.player1.draw(context);
     this.player2.draw(context);
 
-    context.beginPath();
-    context.arc(this.ball.x, this.ball.y, 
-                this.ball.radius, 0, 2*Math.PI, false);
-    context.fillStyle = "rgba(255, 255, 255, " + this.ball.alpha + ")";
-    context.fill();
+    this.ball.draw(context);
 };
